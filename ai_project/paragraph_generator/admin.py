d07147f870cd54ec58c590ed6f8efbdc6dc7ec17
sa001gar <kundusagar233@gmail.com>
2024-10-26 23:09:02 +0530

    Update : After Testing with 36 users. | Database Update

--- ai_project/paragraph_generator/admin.py
+++ ai_project/paragraph_generator/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import GeneratedContent, UserApiUsage
 # Register your models here.
+admin.site.register(GeneratedContent)
+admin.site.register(UserApiUsage)
